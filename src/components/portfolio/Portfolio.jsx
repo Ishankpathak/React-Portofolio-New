5d2b3c54ff874d661a0a02b748036df28298a79c
Ishankpathak <pathakishank4@gmail.com>
2023-07-14 22:46:15 +0530

    Ecommerce app uploaded

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,46 +1,16 @@
 import React from "react";
 import "./portfolio.css";
-import Img1 from "../../assets/google.png";
-import Img2 from "../../assets/crypto.jpg";
-import blog from "../../assets/blog.jpg";
-import Img4 from "../../assets/editor-min.png";
 
-const data = [
-  {
-    id: 1,
-    image: blog,
-    title: "Mern Blog App",
-    github: "https://github.com/Ishankpathak/MERN_BLOG_APP_NEW",
-    demo: "https://blog-app-new.onrender.com",
-  },
-  {
-    id: 1,
-    image: Img1,
-    title: "Google Clone using react",
-    github: "https://github.com/Ishankpathak/Google_Clone",
-    demo: "https://google-apis-clone.netlify.app",
-  },
-  {
-    id: 2,
-    image: Img2,
-    title: "Crypto Currency Clone ",
-    github: "https://github.com/Ishankpathak/crypto_app-chakra_ui",
-    demo: "https://suncrypto.netlify.app",
-  },
-  {
-    id: 3,
-    image: Img4,
-    title: "Text Editor Using React",
-    github: "https://github.com/Ishankpathak/TextEditor",
-    demo: "https://multitexteditor.netlify.app/",
-  },
-];
+import { data } from "./Project_data.js";
+
+
 
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
+      <h3 className="message">There can be sometime little problems in rendering as i have used free server</h3>
       <div className="container portfolio__container">
         {data.map(({ id, image, title, github, demo }) => {
           return (
---
+++ src/components/portfolio/Project_data.js
@@ -0,0 +1,42 @@
+import Img1 from "../../assets/google.png";
+import Img2 from "../../assets/crypto.jpg";
+import blog from "../../assets/blog.jpg";
+import Img4 from "../../assets/editor-min.png";
+import Ecom from "../../assets/ecom.png";
+export const data = [
+  {
+    id: 1,
+    image: blog,
+    title: "Mern Blog App",
+    github: "https://github.com/Ishankpathak/MERN_BLOG_APP_NEW",
+    demo: "https://blog-app-new.onrender.com",
+  },
+  {
+    id: 2,
+    image: Ecom,
+    title: "Mern Ecommerce App",
+    github: "https://github.com/Ishankpathak/E-commerce-Mantra",
+    demo: "https://e-comm-mantra.onrender.com/",
+  },
+  {
+    id: 3,
+    image: Img1,
+    title: "Google Clone using react",
+    github: "https://github.com/Ishankpathak/Google_Clone",
+    demo: "https://google-apis-clone.netlify.app",
+  },
+  {
+    id: 4,
+    image: Img2,
+    title: "Crypto Currency Clone ",
+    github: "https://github.com/Ishankpathak/crypto_app-chakra_ui",
+    demo: "https://suncrypto.netlify.app",
+  },
+  {
+    id: 5,
+    image: Img4,
+    title: "Text Editor Using React",
+    github: "https://github.com/Ishankpathak/TextEditor",
+    demo: "https://multitexteditor.netlify.app/",
+  },
+];

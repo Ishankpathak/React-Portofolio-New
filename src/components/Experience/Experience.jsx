55e199615d9cf7913136e6bb198dc3b871509d15
Ishankpathak <pathakishank4@gmail.com>
2023-08-16 09:18:04 +0530

    Some changes in animation

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -18,142 +18,140 @@ const Experience = () => {
         My Experience
       </h2>
       <div className="container experience__container">
-        <div
-          className="experience__frontend"
-          data-aos="fade-up"
-          data-aos-anchor-placement="top-center"
-        >
-          <h3>Frontend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>HTML</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+        <div data-aos="fade-right">
+          <div className="experience__frontend">
+            <h3>Frontend Development</h3>
+            <div className="experience__content">
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>HTML</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>CSS</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>CSS</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>JavaScript</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>JavaScript</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Bootstrap</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>Bootstrap</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Tailwind</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>Tailwind</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>ReactJS</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>ReactJS</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+            </div>
           </div>
         </div>
 
         {/* END OF FRONTEND */}
 
-        <div
-          className="experience__backend"
-          data-aos="fade-up"
-          data-aos-anchor-placement="top-center"
-        >
-          <h3>Backend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Node JS</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+        <div data-aos="fade-right">
+          <div className="experience__backend">
+            <h3>Backend Development</h3>
+            <div className="experience__content">
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>Node JS</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>MongoDB</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>MongoDB</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>PHP</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>PHP</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>MySQL</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>MySQL</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Java</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>Java</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Express</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>Express</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+            </div>
           </div>
         </div>
 
         {/* End of backend */}
 
-        <div className="experience__backend" data-aos="fade-up" data-aos-anchor-placement="top-center">
-          <h3>Other Skills</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>SEO</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
+        <div data-aos="fade-right">
+          <div className="experience__backend">
+            <h3>Other Skills</h3>
+            <div className="experience__content">
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>SEO</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
 
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Git & Github</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
+              <article className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>Git & Github</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+            </div>
           </div>
         </div>
       </div>
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,33 +14,21 @@ const About = () => {
   }, []);
   return (
     <section id="about">
-      <h5 data-aos="fade-up" data-aos-anchor-placement="top-center">
-        Get To Know
-      </h5>
-      <h2 data-aos="fade-up" data-aos-anchor-placement="top-center">
-        About Me
-      </h2>
+      <h5 data-aos="fade-up">Get To Know</h5>
+      <h2 data-aos="fade-up">About Me</h2>
 
-      <div
-        className="Image"
-        data-aos="fade-up"
-        data-aos-anchor-placement="top-center"
-      >
+      <div className="Image" data-aos="fade-up">
         <div>
           <img src={ME} alt="Ishank Pathak" style={{ width: "15rem" }} />
         </div>
 
-        <div
-          className="Image"
-          data-aos="fade-up"
-          data-aos-anchor-placement="top-center"
-        >
-          <div className="about__content">
+        <div className="Image">
+          <div className="about__content" data-aos="fade-up">
             <div className="about__cards">
               <article className="about__card">
                 <FaAward className="about__icon" />
-                <h5>Experience</h5>
-                <small>Knowledgeable </small>
+                <h5 data-aos="fade-left">Experience</h5>
+                <small data-aos="fade-right">Knowledgeable </small>
               </article>
             </div>
           </div>
@@ -49,8 +37,8 @@ const About = () => {
             <div className="about__cards">
               <article className="about__card">
                 <FiUsers className="about__icon" />
-                <h5>Clients</h5>
-                <small>10+ happy Clients</small>
+                <h5 data-aos="fade-left">Clients</h5>
+                <small data-aos="fade-right">10+ happy Clients</small>
               </article>
             </div>
           </div>
@@ -59,8 +47,8 @@ const About = () => {
             <div className="about__cards">
               <article className="about__card">
                 <BsFillFolderFill className="about__icon" />
-                <h5>Project</h5>
-                <small>50+ completed</small>
+                <h5 data-aos="fade-left">Project</h5>
+                <small data-aos="fade-right">50+ completed</small>
               </article>
             </div>
           </div>

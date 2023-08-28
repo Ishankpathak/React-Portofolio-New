6dbf7d3c5c8e43d45de14eb24557fbe0a2c5c689
Ishankpathak <pathakishank4@gmail.com>
2023-08-28 09:42:41 +0530

    Added typing animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import "./header.css";
 import My from "../../assets/my.png";
 import CTA from "./CTA";
 import HeaderSocials from "./HeaderSocials";
+import Typewriter from "typewriter-effect";
 
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -12,11 +13,20 @@ const Header = () => {
     Aos.init();
   }, []);
   return (
-    <header data-aos="flip-left" data-aos-delay="100" data-aos-anchor=".div" >
+    <header data-aos="flip-left" data-aos-delay="100" data-aos-anchor=".div">
       <div className="container header__container">
         <h5>Hello I'm </h5>
         <h1>Ishank Pathak</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+
+        <Typewriter
+          options={{
+            strings: ["FullStack Developer ", "Programmer"],
+            autoStart: true,
+            loop: true,
+          }}
+          className="mt-5"
+        />
+
         <CTA />
         <HeaderSocials />
 

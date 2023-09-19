e9df228f1c2989f432891454f86a3c5c8f519a14
Ishankpathak <pathakishank4@gmail.com>
2023-09-19 18:37:26 +0530

    Youtube CLone added

--- src/components/portfolio/Project_data.js
+++ src/components/portfolio/Project_data.js
@@ -5,51 +5,59 @@ import Img4 from "../../assets/editor-min.png";
 import Ecom from "../../assets/ecom.png";
 import Weather from "../../assets/weatherapp.png";
 import MovieApp from "../../assets/movie.png";
+import YoutubeImg from "../../assets/youtube-img.png";
 export const data = [
   {
     id: 1,
+    image: YoutubeImg,
+    title: "Youtube Clone",
+    github: "https://github.com/Ishankpathak/youtube_cloneNew",
+    demo: "https://youtubes-mini.netlify.app",
+  },
+  {
+    id: 2,
     image: blog,
     title: "Mern Blog App",
     github: "https://github.com/Ishankpathak/MERN_BLOG_APP_NEW",
     demo: "https://blog-app-new.onrender.com",
   },
   {
-    id: 2,
+    id: 3,
     image: Ecom,
     title: "Mern Ecommerce App",
     github: "https://github.com/Ishankpathak/E-commerce-Mantra",
     demo: "https://e-comm-mantra.onrender.com/",
   },
   {
-    id: 3,
+    id: 4,
     image: Img1,
     title: "Google Clone using react",
     github: "https://github.com/Ishankpathak/Google_Clone",
     demo: "https://google-apis-clone.netlify.app",
   },
   {
-    id: 4,
+    id: 5,
     image: Img2,
     title: "Crypto Currency Clone ",
     github: "https://github.com/Ishankpathak/crypto_app-chakra_ui",
     demo: "https://suncrypto.netlify.app",
   },
   {
-    id: 5,
+    id: 6,
     image: MovieApp,
     title: "Movie Search App",
     github: "https://github.com/Ishankpathak/MovieAppOmdb",
     demo: "https://movistmania.netlify.app",
   },
   {
-    id: 6,
+    id: 7,
     image: Weather,
     title: "Weather App Using React",
     github: "https://github.com/Ishankpathak/TextEditor",
     demo: "https://weathers-updates.netlify.app/",
   },
   {
-    id: 7,
+    id: 8,
     image: Img4,
     title: "Text Editor Using React",
     github: "https://github.com/Ishankpathak/TextEditor",
